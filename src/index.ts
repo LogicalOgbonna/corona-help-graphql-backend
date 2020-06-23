581a150386613584f91887cf9bbcd5e84a255969
LogicalOgbonna <arinzeogbo@gmail.com>
2020-06-23 01:12:34 +0100

    added dynamic port for heroku

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,6 @@ app.use("/graphql", expressGraphQL({
     graphiql: true
 }));
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log('Server is running on port 4000..');
 });
